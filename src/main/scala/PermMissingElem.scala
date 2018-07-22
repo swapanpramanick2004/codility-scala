b9601e8e161772d31bdc7d04f1091ff3f3e0424f
Swapan Pramanick <swpraman@cisco.com>
2018-07-23 03:09:49 +0530

    Lesson3: PermMissingElem

--- src/main/scala/PermMissingElem.scala
+++ src/main/scala/PermMissingElem.scala
@@ -9,17 +9,20 @@ object PermMissingElem extends App {
     val n = a.length
     val sumExpected = ((n+1)/2.0)*(n+2)
     val sumActual = sumN(n - 1, 0)
-    println(sumExpected)
-    println(sumActual)
+    //println(sumExpected)
+    //println(sumActual)
     (sumExpected - sumActual).toInt
   }
 
+  // Small test case
+  println(solution(Array(1, 3, 2, 7, 6, 8, 5)))
+
+  // To create large test case
   val n = 1000001
   val r = new Random().nextInt(n)
   @tailrec
   def generate(n:Int, list:List[Int]):List[Int] = if ( n == r) generate(n - 1, list) else if (n >= 0) generate(n - 1, n::list) else list
 
   val list = generate(n, Nil)
-  println(solution(Array(1, 3, 2, 7, 6, 8, 5)))
-  //println(solution(list.toArray))
+  println(solution(list.toArray))
 }
